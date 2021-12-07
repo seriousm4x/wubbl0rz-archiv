3d8044ed66ef53acb1f715725a1390ab8eaa4347
Maxi Quoß <maxi@quoss.org>
2021-12-07 20:22:26 +0100

    django 4.0: enable redis caching

--- wubbl0rz_archiv/wubbl0rz_archiv/settings.py
+++ wubbl0rz_archiv/wubbl0rz_archiv/settings.py
@@ -90,6 +90,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://localhost:6379',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
