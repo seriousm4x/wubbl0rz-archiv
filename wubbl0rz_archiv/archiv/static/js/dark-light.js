a7dfbeb65b389a562c97dffae0337771913a4396
Maxi Quoß <maxi@quoss.org>
2021-12-08 15:29:51 +0100

    changed: better dark mode support

--- wubbl0rz_archiv/archiv/static/js/dark-light.js
+++ wubbl0rz_archiv/archiv/static/js/dark-light.js
@@ -1,40 +1,29 @@
 function load() {
-    let darkModeState = false;
+    let currentModeState = false;
+    const toggleBtn = document.querySelector(".toggle-dark");
+    const preferesDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-    const toggle = document.querySelector(".toggle");
-
-    // MediaQueryList object
-    const useDark = window.matchMedia("(prefers-color-scheme: dark)");
-
-    // Toggles the "dark-mode" class
     function toggleDarkMode(state) {
         document.documentElement.classList.toggle("dark-mode", state);
-        darkModeState = state;
-        toggle.checked = !state;
-    }
-
-    // Sets localStorage state
-    function setDarkModeLocalStorage(state) {
         localStorage.setItem("dark-mode", state);
+        currentModeState = state;
+        toggleBtn.classList.toggle("active", state);
     }
 
-    // Initial setting
-    toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+    if (localStorage.getItem("dark-mode") === null) {
+        toggleDarkMode(preferesDark.matches);
+    } else {
+        toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+    }
 
-    // Listen for changes in the OS settings.
-    // Note: the arrow function shorthand works only in modern browsers, 
-    // for older browsers define the function using the function keyword.
-    useDark.addEventListener("change", e => {
+    preferesDark.addEventListener("change", e => {
         toggleDarkMode(e.matches);
     })
 
-    // Toggles the "dark-mode" class on click and sets localStorage state
-    toggle.addEventListener("click", () => {
-        darkModeState = !darkModeState;
-        toggleDarkMode(darkModeState);
-        setDarkModeLocalStorage(darkModeState);
+    toggleBtn.addEventListener("click", () => {
+        currentModeState = !currentModeState;
+        toggleDarkMode(currentModeState);
     });
-
 }
 
 window.addEventListener("DOMContentLoaded", load);
