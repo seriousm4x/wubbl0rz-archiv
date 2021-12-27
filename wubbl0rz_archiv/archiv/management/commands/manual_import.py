d32ed66b172f39c7ed751347d903f53814c122d0
Maxi Quoß <maxi@quoss.org>
2021-12-27 12:32:27 +0100

    updated: manual import

--- wubbl0rz_archiv/archiv/management/commands/manual_import.py
+++ wubbl0rz_archiv/archiv/management/commands/manual_import.py
@@ -7,7 +7,6 @@ from archiv.models import Vod
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.timezone import make_aware
-from pymediainfo import MediaInfo
 
 
 class Command(BaseCommand):
@@ -17,42 +16,48 @@ class Command(BaseCommand):
                 continue
 
             id, _ = os.path.splitext(f)
-
             print("importing:", id)
-
             ts = os.path.join(settings.MEDIA_ROOT, id + ".ts")
+            with open(os.path.join(settings.MEDIA_ROOT, id + ".json"), "r", encoding="utf-8") as info:
+                data = json.load(info)
+                title = data["title"]
+                timestamp = data["timestamp"]
+                fps = data["fps"]
 
-            cmd = ["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of",
-                "default=noprint_wrappers=1:nokey=1", os.path.join(settings.MEDIA_ROOT, ts)]
-            proc = subprocess.Popen(
-                cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-            out, _ = proc.communicate()
-            duration = float(out.decode().strip())
+            duration, resolution, filesize = self.get_metadata(ts)
+            self.update_db(id, title, duration, timestamp,
+                           resolution, fps, filesize)
 
-            media_info = MediaInfo.parse(ts)
-            for track in media_info.tracks:
-                if track.track_type == "Video":
-                    resolution = f"{track.width}x{track.height}"
+    def get_metadata(self, ts):
+        # duration
+        cmd = ["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of",
+               "default=noprint_wrappers=1:nokey=1", ts]
+        proc = subprocess.Popen(
+            cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        out, _ = proc.communicate()
+        duration = float(out.decode().strip())
 
-            bitrate = media_info.general_tracks[0].to_data()[
-                "overall_bit_rate"]
+        # resolution
+        cmd = ["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries",
+               "stream=width,height", "-of", "csv=s=x:p=0", ts]
+        proc = subprocess.Popen(
+            cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        out, _ = proc.communicate()
+        resolution = out.decode().splitlines()[0].strip()
 
-            filesize = os.path.getsize(ts)
+        # filesize
+        filesize = os.path.getsize(ts)
 
-            with open(os.path.join(settings.MEDIA_ROOT, id + ".json"), "r", encoding="utf-8") as fh:
-                info_dict = json.load(fh)
-            title = info_dict["title"]
-            timestamp = info_dict["timestamp"]
-            fps = info_dict["fps"]
+        return duration, resolution, filesize
 
-            Vod.objects.update_or_create(
-                filename=id,
-                defaults={
-                    "title": title,
-                    "duration": duration,
-                    "date": make_aware(datetime.fromtimestamp(timestamp)),
-                    "resolution": resolution,
-                    "bitrate": bitrate,
-                    "fps": fps,
-                    "size": filesize
-                })
+    def update_db(self, id, title, duration, timestamp, resolution, fps, filesize):
+        Vod.objects.update_or_create(
+            filename=id,
+            defaults={
+                "title": title,
+                "duration": duration,
+                "date": make_aware(datetime.fromtimestamp(timestamp)),
+                "resolution": resolution,
+                "fps": fps,
+                "size": filesize
+            })
