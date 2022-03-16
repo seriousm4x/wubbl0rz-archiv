d0b2fabd2c1c981a1596ca038ca2bf08eca0819c
Maxi Quoß <maxi@quoss.org>
2022-03-16 18:45:40 +0100

    api: custom pagination response

--- wubbl0rz_archiv/main/serializers.py
+++ wubbl0rz_archiv/main/serializers.py
@@ -17,6 +17,17 @@ class StandardResultsSetPagination(PageNumberPagination):
     max_page_size = 500
     page_size_query_param = "page_size"
 
+    def get_paginated_response(self, data):
+        return Response({
+            "links": {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link()
+            },
+            "count": self.page.paginator.count,
+            "total_pages": self.page.paginator.num_pages,
+            "results": data
+        })
+
 
 class VodSerializer(serializers.HyperlinkedModelSerializer):
     clip_set = serializers.SlugRelatedField(
