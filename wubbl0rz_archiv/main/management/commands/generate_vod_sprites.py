7771bafbd3bb8703f1e0ce03c0dd34255b11a5e2
Maxi Quoß <maxi@quoss.org>
2022-04-17 18:12:01 +0200

    skip vod if dir exists

--- wubbl0rz_archiv/main/management/commands/generate_vod_sprites.py
+++ wubbl0rz_archiv/main/management/commands/generate_vod_sprites.py
@@ -17,7 +17,9 @@ class Command(BaseCommand):
             id = f.replace("-segments", "")
             print("create sprites for vod:", id)
             sprite_dir = os.path.join(self.vod_dir, id + "-sprites")
-            if not os.path.isdir(sprite_dir):
+            if os.path.isdir(sprite_dir):
+                return
+            else:
                 os.mkdir(sprite_dir)
             cmd = ["ffmpeg", "-i", os.path.join(self.vod_dir, f, id+".m3u8"), "-vf", "fps=1/20,scale=-1:90,tile",
                    "-c:v", "libwebp", os.path.join(sprite_dir, id+r"_%03d.webp")]
