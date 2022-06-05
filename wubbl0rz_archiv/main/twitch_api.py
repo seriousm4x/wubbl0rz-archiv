3637fcb9b5a3eb913030e6e8ebf3b950ac1913cb
Maxi Quoß <maxi@quoss.org>
2022-06-05 21:18:35 +0200

    add discord webhook notification

--- wubbl0rz_archiv/main/twitch_api.py
+++ wubbl0rz_archiv/main/twitch_api.py
@@ -1,6 +1,8 @@
 import datetime
+import os
 
 import requests
+from discord_webhook import DiscordEmbed, DiscordWebhook
 from django.utils import timezone
 
 from main.models import ApiStorage
@@ -54,20 +56,42 @@ class TwitchApi:
 
     def check_live(self):
         bearer = self.get_bearer()
-        url = f"https://api.twitch.tv/helix/streams?user_id={self.broadcaster_id}"
         helix_header = {
             "Client-ID": self.client_id,
             "Authorization": f"Bearer {bearer}",
         }
-        resp = requests.get(url, headers=helix_header).json()
-        if resp["data"]:
+        stream_url = f"https://api.twitch.tv/helix/streams?user_id={self.broadcaster_id}"
+        stream_resp = requests.get(
+            stream_url, headers=helix_header).json()["data"]
+        if len(stream_resp) >= 0:
             is_live = True
+            stream_resp = stream_resp[0]
         else:
             is_live = False
 
-        ApiStorage.objects.update_or_create(
-            broadcaster_id=self.broadcaster_id,
-            defaults={
-                "is_live": is_live
-            }
-        )
+        if is_live != self.apistorage.is_live:
+            ApiStorage.objects.update_or_create(
+                broadcaster_id=self.broadcaster_id,
+                defaults={
+                    "is_live": is_live
+                }
+            )
+
+            if is_live and os.getenv("DISCORD_WEBHOOK"):
+                user_url = f"https://api.twitch.tv/helix/users?id={self.broadcaster_id}"
+                user_resp = requests.get(
+                    user_url, headers=helix_header).json()["data"]
+
+                timestamp = datetime.datetime.strptime(
+                    stream_resp["started_at"], "%Y-%m-%dT%H:%M:%SZ")
+                webhook = DiscordWebhook(url=os.getenv(
+                    "DISCORD_WEBHOOK"), content=f"Auf gehts @everyone, {stream_resp['user_name']} macht nun Streamstelz")
+                embed = DiscordEmbed(
+                    title=f"{stream_resp['title']}, https://twitch.tv/{stream_resp['user_login']}", description=stream_resp["game_name"], color="f28e2b")
+                embed.set_author(
+                    name=stream_resp["user_name"], url=f"https://twitch.tv/{stream_resp['user_login']}", icon_url=user_resp[0]["profile_image_url"])
+                embed.set_image(url=stream_resp["thumbnail_url"].replace(
+                    r"{width}", "1280").replace(r"{height}", "720"))
+                embed.set_timestamp(datetime.datetime.timestamp(timestamp))
+                webhook.add_embed(embed)
+                webhook.execute()
