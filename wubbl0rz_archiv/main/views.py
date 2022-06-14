7a23b80ccd96eab65aedb5fee12a10b5d818011c
Maxi Quoß <maxi@quoss.org>
2022-06-14 23:16:58 +0200

    kill ffmpeg after client cancled download

--- wubbl0rz_archiv/main/views.py
+++ wubbl0rz_archiv/main/views.py
@@ -41,11 +41,15 @@ def download(request, type, uuid):
             data = proc.stdout.read(1024*4)
             if not data:
                 break
-            ff_queue.put(data)
-
-    proc = subprocess.Popen(
-        cmd, bufsize=1024*4, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    t = Thread(target=read_output, args=(proc,))
+            # try to put new data in queue. kill after timeout if no data has been pulled => client disconnected
+            # a client must have downloaded data size within timeout, else the download quits
+            try:
+                ff_queue.put(data, timeout=60*5)
+            except queue.Full:
+                proc.kill()
+                with ff_queue.mutex:
+                    ff_queue.queue.clear()
+                break
 
     def iterator():
         t.start()
@@ -59,6 +63,10 @@ def download(request, type, uuid):
                 yield data
             except queue.Empty:
                 pass
+
+    proc = subprocess.Popen(
+        cmd, bufsize=1024*4, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    t = Thread(target=read_output, args=(proc,))
 
     response = StreamingHttpResponse(iterator(), content_type="video/mp4")
     response["Content-Disposition"] = f"attachment; filename={slugify(obj.date)}-{slugify(obj.title)}.mp4"
