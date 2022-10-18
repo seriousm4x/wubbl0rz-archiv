a0f2fec3ef3cee9e585ec5036aed060808edb514
Maxi Quoß <maxi@quoss.org>
2022-10-18 22:32:08 +0200

    change tag to span

--- pkg/queries/vod_query.go
+++ pkg/queries/vod_query.go
@@ -124,7 +124,7 @@ func GetVodsFullText(foundVods *[]map[string]interface{}, query string, paginati
 	var vod models.Vod
 
 	result := database.DB.Model(&vod).
-		Select("vods.uuid, vods.title, vods.duration, vods.date, vods.viewcount, vods.filename, vods.resolution, vods.fps, vods.size, ts_headline('german', vods.transcript, websearch_to_tsquery('german', ?) || websearch_to_tsquery('simple', ?)) as matches, ts_rank(vods.transcript_vector, websearch_to_tsquery('german', ?)) + ts_rank(vods.transcript_vector, websearch_to_tsquery('simple', ?)) as rank", query, query, query, query).
+		Select("vods.uuid, vods.title, vods.duration, vods.date, vods.viewcount, vods.filename, vods.resolution, vods.fps, vods.size, ts_headline('german', vods.transcript, websearch_to_tsquery('german', ?) || websearch_to_tsquery('simple', ?), 'StartSel=<span>,StopSel=</span>') as matches, ts_rank(vods.transcript_vector, websearch_to_tsquery('german', ?)) + ts_rank(vods.transcript_vector, websearch_to_tsquery('simple', ?)) as rank", query, query, query, query).
 		Where("publish = ? and vods.transcript_vector @@ websearch_to_tsquery('german', ?) or vods.transcript_vector @@ websearch_to_tsquery('simple', ?)", true, query, query).
 		Order("rank desc").
 		Find(foundVods).
