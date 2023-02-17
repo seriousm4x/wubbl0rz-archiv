6e74989c08bc6a07aeba98b366e9189280fe310e
Maxi Quoß <maxi@quoss.org>
2023-02-17 15:52:44 +0100

    dont auto migrate generated fields

--- pkg/models/vod_model.go
+++ pkg/models/vod_model.go
@@ -5,7 +5,7 @@ import "time"
 type Vod struct {
 	UUID             string     `gorm:"primaryKey;uniqueIndex" json:"uuid" form:"uuid"`
 	Title            string     `gorm:"column:title" json:"title" form:"title"`
-	TitleVector      string     `gorm:"column:title_vector;->;type:tsvector generated always as (setweight(to_tsvector('german',title), 'A') || ' ' || setweight(to_tsvector('english',title), 'B')) stored;default:(-);index:title_idx,type:gin" json:"-" form:"-"`
+	TitleVector      string     `gorm:"-:migration;column:title_vector;->;type:tsvector generated always as (setweight(to_tsvector('german',title), 'A') || ' ' || setweight(to_tsvector('english',title), 'B')) stored;default:(-);index:title_idx,type:gin" json:"-" form:"-"`
 	Duration         int        `json:"duration" form:"duration"`
 	Date             *time.Time `json:"date" form:"date" time_format:"2006-01-02T15:04:05.000Z"`
 	Viewcount        int        `gorm:"default:0" json:"viewcount" form:"viewcount"`
@@ -16,5 +16,5 @@ type Vod struct {
 	Publish          bool       `json:"publish" form:"publish"`
 	Clips            []Clip     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"clips" form:"clips"`
 	Transcript       string     `gorm:"column:transcript;type:text" json:"transcript" form:"transcript"`
-	TranscriptVector string     `gorm:"column:transcript_vector;->;type:tsvector generated always as (setweight(to_tsvector('german',transcript), 'C') || ' ' || setweight(to_tsvector('english',transcript), 'D')) stored;default:(-);index:transcript_idx,type:gin" json:"-" form:"-"`
+	TranscriptVector string     `gorm:"-:migration;column:transcript_vector;->;type:tsvector generated always as (setweight(to_tsvector('german',transcript), 'C') || ' ' || setweight(to_tsvector('english',transcript), 'D')) stored;default:(-);index:transcript_idx,type:gin" json:"-" form:"-"`
 }
