6330282aa85fa03d5cc0066b0052b85118f1c459
seriousm4x <maxi@quoss.org>
2023-11-12 13:40:00 +0100

    only load plausible if hostname matches

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
 	import '../app.css';
+	import { page } from '$app/stores';
 </script>
 
 <Sidebar>
@@ -12,3 +13,7 @@
 		</div>
 	</div>
 </Sidebar>
+
+{#if $page.url.hostname === 'wubbl0rz.tv'}
+	<script defer data-domain="wubbl0rz.tv" src="https://metrics.wubbl0rz.tv/js/script.js"></script>
+{/if}
