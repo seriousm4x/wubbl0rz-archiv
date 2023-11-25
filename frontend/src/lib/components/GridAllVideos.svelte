403409512a6966761245b72fa5f32fdf98fabaf5
seriousm4x <maxi@quoss.org>
2023-11-25 13:39:01 +0100

    remove cursor-pointer from input

--- frontend/src/lib/components/GridAllVideos.svelte
+++ frontend/src/lib/components/GridAllVideos.svelte
@@ -112,7 +112,7 @@
 		<form on:submit|preventDefault={() => search()}>
 			<div class="flex flex-col flex-wrap justify-end gap-2 md:flex-row">
 				<input
-					class="input w-full cursor-pointer rounded-full border-base-content/20 bg-base-300/50 text-base-content/50 drop-shadow-md transition duration-200 hover:border-base-content/50 hover:bg-base-300/80 hover:text-base-content md:max-w-lg"
+					class="input w-full rounded-full border-base-content/20 bg-base-300/50 text-base-content/50 drop-shadow-md transition duration-200 hover:border-base-content/50 hover:bg-base-300/80 hover:text-base-content md:max-w-lg"
 					{placeholder}
 					bind:value={searchValue}
 				/>
