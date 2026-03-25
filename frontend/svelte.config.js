67a6639c533cd95c1305530d75c169a360b6b0c8
Maxi Quoß <maxi@quoss.org>
2026-03-25 15:53:53 +0100

    fix: use Date.now() to set version

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -20,7 +19,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		version: {
-			name: child_process.execSync('git rev-parse HEAD').toString().trim(),
+			name: Date.now().toString(),
 			pollInterval: 10 * 1000 // 10 sec
 		}
 	}
