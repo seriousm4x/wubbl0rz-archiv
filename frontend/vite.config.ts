71947b08597cfe75135e498a4b9921a54aea7b6f
Maxi Quoß <maxi@quoss.org>
2026-05-19 22:55:11 +0200

    use tailwind via vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { vite as vidstack } from 'vidstack/plugins';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [vidstack(), sveltekit()]
+	plugins: [tailwindcss(), vidstack(), sveltekit()]
 });
